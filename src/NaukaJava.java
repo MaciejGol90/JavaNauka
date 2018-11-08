8a869b1ecbb6c9a5b1c2542262c2c23b6df0b3fa
Maciej Golianek <maciek.golianek@gmail.com>
2018-11-08 23:30:10 +0100

    ciąg dalszy
    nauka według kursu udacity

--- src/NaukaJava.java
+++ src/NaukaJava.java
@@ -1,6 +1,17 @@
+ public class NaukaJava {
+             }
 int passengers;
 passsangers = 0;
 passangers = passangers + 5;    
 passangers = passangers - 3;
 passangers = passangers - 1 + 5;
 System.out.println(passangers);
+
+String driver;
+driver = "Hamish";
+int letters = driver.lenght();//ile liter zawiera zmienna driver
+System.out.println(letters); //wyświatla długość zmiennej letters
+ 
+
+String driver;
+driver = "Hamish";
