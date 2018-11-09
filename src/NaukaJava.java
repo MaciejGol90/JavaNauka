a3d388c0185b9c0247a156aa027403fccd67b99e
Maciej Golianek <maciek.golianek@gmail.com>
2018-11-09 11:16:31 +0100

    cd
    nauka według kursu udacity

--- src/NaukaJava.java
+++ src/NaukaJava.java
@@ -1,17 +1,25 @@
  public class NaukaJava {
-             }
+             
 int passengers;
-passsangers = 0;
-passangers = passangers + 5;    
-passangers = passangers - 3;
-passangers = passangers - 1 + 5;
-System.out.println(passangers);
+    passangers = 0;
+    passangers = passangers + 5;    
+    passangers = passangers - 3;
+    passangers = passangers - 1 + 5;
+        System.out.println(passangers);
 
 String driver;
 driver = "Hamish";
 int letters = driver.lenght();//ile liter zawiera zmienna driver
-System.out.println(letters); //wyświatla długość zmiennej letters
- 
+System.out.println(letters);//wyświatla długość zmiennej letter
 
 String driver;
 driver = "Hamish";
+
+String driverFirstName;
+String driverSecondName;
+driverFirstName = "Hamish";
+driverSecondName = "Blake";
+String driverFullName = driverFirstName +" "+ driverSecondName;
+System.out.println(driverFillName);
+ }
+
