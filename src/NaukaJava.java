c26f24730e16e75a61953dc258fa7c44d6ad6c0f
Maciej Golianek <maciek.golianek@gmail.com>
2018-11-13 13:17:45 +0100

    funkcje

--- src/NaukaJava.java
+++ src/NaukaJava.java
@@ -10,61 +10,15 @@ package naukajawy;
  * @author maciejgolianek
  */
 //zad1.
-public class NaukaJawy {
-int x = 5;
-double temperature = 75.6;
-char grade = 'A';
-String name = "Adam";
-// zad2.
-double register = 10.0;
-    register = register + 5;
-    register = register - 2.5;
-    register = register + 10;
-    register = register - 3;
-System.out.println(register);
-//zad3
-int bankBalance = 500;
-bankBalance = bankBalance + 250;
-bankBalance = bankBalance - 100;
-System.out.println(bankBalance);
-//zad4
-System.out.println(2.0 * (5/2)); //=4.0
-// zad5
-int day = 8;
-String month = July;
-//zad6
-String firstName = "Maciej";
-String lastName = "Golianek";
-String fullName = firstName + " " + lastName;
-int lettersInName = lastName.length(lastName) + firstName.length (firstName);
-System.out.println("Hello, my name is " + fullName + ".");
-Syste.out.println("There are " + lettersInName + "letters in my name.");
-//zad 7
-public class celcius{;
-double farenhait = 13.0;
-double celcius = (farenhait - 32) * 5 /9;
-System.out.println(celcius)
+public void println (String x) {
 
-        }
-
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        boolean action = true;
-boolean romance = true;
-boolean horror = false;
-boolean comedy = true;
-
-if (action && romance) {
-    //zawiera romans i akcje
-    System.out.println("Ten film zawiera akcje i romans.");
-    if (comedy || horror) {
-    System.out.println ("Ten film zawiera również komedie lub horror!")
+    if(x == null) {
+    x = "null";
     }
-}
+    try {
+        ensureOpen ();
+        texOut.write(x);
+    } catch (IOException e) {
+     trouble = true;
     }
-    
 }
